0cb09049e90afdb1e7636377e6f824a467f47c17
aliii390 <alli.said.errahmani@gmail.com>
2025-03-19 16:54:56 +0100

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,22 @@
-import './styles/App.css'
-import Header from './Header'
-import MeteoCard from './components/MeteoCard/MeteoCard'
-
-
+import "./styles/App.css";
+import Header from "./Header";
+import MeteoCard from "./components/MeteoCard/MeteoCard.jsx";
 
 function App() {
- 
-
   return (
     <>
-<div className="App">
-
-<Header />
-
-
-<div className="row">
-
-<div className="col s12 m6 push-m3">
-
-<div className="weather card blue-grey darken-1">
-<MeteoCard /> 
-  
-   </div>
-
-   </div>
-
-  </div>
-
-
-</div>
-
-
+      <div className="App">
+        <Header />
+        <div className="row">
+          <div className="col s12 m6 push-m3">
+            <div className="weather card blue-grey darken-1">
+              <MeteoCard />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
